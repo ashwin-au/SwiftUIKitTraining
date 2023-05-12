ffb60a576ded377926849979ca9bf44037963b2a
Ashwin <ashwinau4u@gmail.com>
2023-05-12 13:15:46 +0530

    files added

---
+++ SwiftUIKitTraining/SwiftUIKitTraining/TableViewController/TableViewCells/studentCell.swift
@@ -0,0 +1,16 @@
+//
+//  studentCell.swift
+//  SwiftUIKitTraining
+//
+//  Created by Ashwin A U on 12/05/23.
+//
+
+import UIKit
+
+class studentCell: UITableViewCell {
+    
+    @IBOutlet weak var studentIcon: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var address: UILabel!
+    
+}
